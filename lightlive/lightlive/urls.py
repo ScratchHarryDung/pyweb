e3d03aece4c9e9aa025f82c007a98c31193e5003
Thuydd <thuydd91@gmail.com>
2024-05-12 08:22:38 +0700

    add commen

--- lightlive/lightlive/urls.py
+++ lightlive/lightlive/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from homepage import views as homepage
 
 # đoạn   FROM DJANGO.URLS IMPORT PATH   là mình lấy cái gì ra ạ? tại sao lại có include ở phía sau làm gì ạ?
-# đoạn   FROM HOMEPAGE IMPORT VIEW AS HOMEPAGE   thì cái khúc as homepage có cần thiết ko ạ? mạng bảo đó là tên, thì e thắc mắc là tên của cái gì ạ?
+# đoạn   FROM HOMEPAGE IMPORT VIEW AS HOMEPAGE   thì cái khúc as homepage có cần thiết ko ạ? mạng bảo đó là tên, thì e thắc mắc là tên của cái gì ạ? => đây là tên do xem đặt, em có thể đặt nó là 1 cái tên bất kì
+
 
 
 urlpatterns = [
